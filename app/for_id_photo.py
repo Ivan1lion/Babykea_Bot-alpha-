4d4d5fc7ac611bbcf3a4b074850d7bd9015439dc
Ivan Art <tobedrive@yandex.ru>
2026-02-06 16:15:57 +0300

    Проверка гита

--- app/for_id_photo.py
+++ app/for_id_photo.py
@@ -7,6 +7,7 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 
+
 API_TOKEN = os.getenv("TOKEN")
 if not API_TOKEN:
     raise ValueError("Переменная окружения TOKEN не установлена!")
